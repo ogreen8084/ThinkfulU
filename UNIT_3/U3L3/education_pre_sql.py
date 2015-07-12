493daa924c640e61eb8fa4d6112572c740b6d21e
ogeezy <fsug@yahoo.com>
2015-07-12 08:48:33 -0400

    Update education_pre_sql.py

--- UNIT_3/U3L3/education_pre_sql.py
+++ UNIT_3/U3L3/education_pre_sql.py
@@ -10,7 +10,9 @@ con = lite.connect('GDP_EDUCATION.db')
 cur = con.cursor()
 
 with con:
-	cur.execute('CREATE TABLE gdp (country_name TEXT, _1999 REAL, _2000 REAL, _2001 REAL, _2002 REAL, _2003 REAL, _2004 REAL, _2005 REAL, _2006 REAL, _2007 REAL, _2008 REAL, _2009 REAL, _2010 REAL);')
+	cur.execute('CREATE TABLE gdp (country_name TEXT, _1999 REAL, _2000 REAL, \
+	_2001 REAL, _2002 REAL, _2003 REAL, _2004 REAL, _2005 REAL, _2006 REAL, _2007 REAL, \
+	_2008 REAL, _2009 REAL, _2010 REAL);')
 
 with open('ny.gdp.mktp.cd_Indicator_en_csv_v2/ny.gdp.mktp.cd_Indicator_en_csv_v2.csv','rU') as inputFile:
     next(inputFile) # skip the first three lines
@@ -20,7 +22,9 @@ with open('ny.gdp.mktp.cd_Indicator_en_csv_v2/ny.gdp.mktp.cd_Indicator_en_csv_v2
     inputReader = csv.reader(inputFile)
     for line in inputReader:
         with con:
-            cur.execute('INSERT INTO gdp (country_name, _1999, _2000, _2001, _2002, _2003, _2004, _2005, _2006, _2007, _2008, _2009, _2010) VALUES ("' + line[0] + '","' + '","'.join(line[43:-5]) + '");')
+            cur.execute('INSERT INTO gdp (country_name, _1999, _2000, _2001, _2002,\
+            _2003, _2004, _2005, _2006, _2007, _2008, _2009, _2010) VALUES ("' + line[0]\
+            + '","' + '","'.join(line[43:-5]) + '");')
 
 con.close()
 
