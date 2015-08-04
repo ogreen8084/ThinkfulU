05b9bdb85efc29c99e3064dd4586ac9eef5beb73
ogeezy <fsug@yahoo.com>
2015-08-04 18:48:07 -0400

    added new file

--- unit_4/u4l2/spam_csv_convert.py
+++ unit_4/u4l2/spam_csv_convert.py
@@ -10,14 +10,14 @@ ham = glob.glob("enron1/ham/*.txt")
 tdm = textmining.TermDocumentMatrix()
 tdm1 = textmining.TermDocumentMatrix()
 
-for item in spam[0:100]:
+for item in spam[0:120]:
 	f = open(item)
 	words = f.read()
 	tdm.add_doc(words)
 tdm.write_csv('spam.csv', cutoff=2)
 
 
-for item1 in ham[0:100]:
+for item1 in ham[0:294]:
 	f = open(item1)
 	words = f.read()
 	tdm1.add_doc(words)
